7881ffd4ac84e103ec53fccb228b097b8769486a
Aalaap Ghag <a.ghag@easternenterprise.com>
2016-12-15 16:35:21 +0530

    Update test case with more tests

--- test.js
+++ test.js
@@ -1,13 +1,24 @@
-/* This program is free software. It comes without any warranty, to
-     * the extent permitted by applicable law. You can redistribute it
-     * and/or modify it under the terms of the Do What The Fuck You Want
-     * To Public License, Version 2, as published by Sam Hocevar. See
-     * http://www.wtfpl.net/ for more details. */
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Aalaap Ghag. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
 var midPad = require("./");
 var test = require("tape");
 
-test('simple test', function (assert) {
-  assert.plan(1);
-  assert.strictEqual(leftPad('foobar', 1, ' '), 'f o o b a r');
+test('basic test', function (assert) {
+  assert.plan(3);
+  assert.strictEqual(midPad('foobar', 1, ' '), 'f o o b a r');
+  assert.strictEqual(midPad('foobar', 3, ' '), 'f   o   o   b   a   r');
+  assert.strictEqual(midPad('foobar', 5, ' '), 'f     o     o     b     a     r');
+});
+
+test('optionals test', function (assert) {
+  assert.plan(2);
+  assert.strictEqual(midPad('foobar', 1), 'f o o b a r');
+  assert.strictEqual(midPad('foobar'), 'foobar');
 });
 
